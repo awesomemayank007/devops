b5a8762089947c4b0c96dd9406cf59a15db40aa2
awesomemayank007 <32386066+awesomemayank007@users.noreply.github.com>
2017-10-31 18:43:36 +0530

    update script

--- disable_port_security.sh
+++ disable_port_security.sh
@@ -3,6 +3,8 @@
 echo "============================================ START =================================================="
 a=`neutron port-list | grep -w $1 |cut -d"|" -f2`
 echo "============================================ OLD neutron port-show $a =================================================="
+neutron port-show $a
+
 neutron port-update --no-security-groups  $a
 neutron port-update --port-security-enabled=false  $a
 
